69f0ac373dc44fa2b7990bc548bdda22c820cde4
Piotrek <you@example.com>
2023-03-28 10:07:37 +0200

    Zadanie3_Fabryka_Czekolady

---
+++ Zadanie3_Fabryka_Czekolady/IsolatingCoolingSystem.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fabryka_batonow
+{
+    public class IsolatingCoolingSystem
+    {
+        public void Fill() { }
+        public void Vent() { }
+
+    }
+}
